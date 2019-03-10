98bac6d1d1b314b0924cae8c4cd45b57ce4cc14d
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-03-10 12:29:45 -0300

    Knight

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -9,6 +9,7 @@ import BoardGame.Piece;
 import BoardGame.Position;
 import Chess_Pieces.Bishop;
 import Chess_Pieces.King;
+import Chess_Pieces.Knight;
 import Chess_Pieces.Pawn;
 import Chess_Pieces.Rook;
 
@@ -199,9 +200,11 @@ public class ChessMatch {
 	//Iniciar a partida de xadrez colocando as peças no tabuleiro;
 	private void initialSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -213,9 +216,11 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
